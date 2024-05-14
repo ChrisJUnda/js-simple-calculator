189bf4b5f28363754165f6e814bb77d009bca0d8
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-05-14 12:03:55 +0200

    [UPD] Milestone-3 done

--- esercizio/js/main.js
+++ esercizio/js/main.js
@@ -9,9 +9,12 @@ const numbers = document.querySelector('.numbers')
 
 const operators = document.querySelector('.operators')
 
+const uguale = document.getElementById('uguale')
+
+let myResult = 0
 let myOperator;
 let firstNumber;
-let secondNumber
+let secondNumber;
 
 
 
@@ -19,12 +22,56 @@ numbers.addEventListener('click', tastiNumeri )
 
 operators.addEventListener('click', tastiOperatori )
 
+uguale.addEventListener('click', function(event){
+    event.stopPropagation()
+
+    console.log('ho cliccato su uguale')
+
+    secondNumber = result.innerText
+
+    myResult = result.innerText = calcoli(Number(firstNumber), Number(secondNumber), myOperator)
+
+
+
+    
+    
+
+    
+
+})
+
 
 /********
  Funzioni
  */
 
+ function calcoli(num1, num2, op) {
+
+    switch(op){
+        
+        case '+':
+        default:
+            console.log('somma')
+            return num1 + num2;
+
+        case '-':
+            console.log('sottrai')
+            return num1 - num2;
+        
+        case '*':
+            console.log('moltiplica')
+            return num1 * num2;
+        
+        case '/':
+            console.log('dividi')
+            return num1 / num2;
+    }
+ }
+
  function tastiNumeri(event) {
+
+
+    console.log('sono evento numbers / padre')
 
 
     const currentElement = event.target
@@ -38,9 +85,10 @@ operators.addEventListener('click', tastiOperatori )
 
     
 
-    if (result.innerText === '0') {
+    if (result.innerText === '0' || myResult !== 0) {
 
         result.innerText = currentElement.innerText
+        myResult = 0
 
     }  else {
         result.innerText += currentElement.innerText
@@ -50,13 +98,6 @@ operators.addEventListener('click', tastiOperatori )
 
 function tastiOperatori(event){
 
-    /*
-    console.log(event.target)
-
-    console.log('ho cliccato su un operatore')
-
-    console.log(event.target.dataset.operator)
-    */
 
     const currentElement = event.target
 
